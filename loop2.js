6cd7171d0841f2186a36473461947906a145b1a8
DireRaven93 <115950779+DireRaven93@users.noreply.github.com>
2022-10-23 18:37:32 -0500

    Add files via upload

---
+++ loop2.js
@@ -0,0 +1,25 @@
+function loop1()
+{
+    for (var i = 1; i< 10; i++)
+    {
+        alert("Can You Count How Many Times This Comes Up?")
+    }
+}
+function loop2()
+{
+    var i = 1;
+    while (i < 7)
+    {
+        alert("Can You Count How Many Times This Comes Up?")
+        i++; // i = i + 1;
+    }
+}
+function loop3()
+{
+    var i = 5;
+    do 
+    {
+        alert("This is the only one")
+        i++; // i = i + 1;
+    } while (i < 4)
+}
